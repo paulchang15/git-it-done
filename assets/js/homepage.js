c26c36fe1911044af72a154e20ecac6dadadf2d0
Paul Chang <paulchang959@gmail.com>
2020-12-10 17:49:40 -0800

    code update for error

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -54,9 +54,10 @@ var displayRepos = function(repos, searchTerm) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     // create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-
+    repoEl.setAttribute("href", "./single-repo.html");
+    
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
@@ -76,6 +77,11 @@ var displayRepos = function(repos, searchTerm) {
       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
 
+    // create a link for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+    
     // append to container
     repoEl.appendChild(statusEl);
     
